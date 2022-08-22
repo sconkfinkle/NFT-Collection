954ca8bb467f50763d6b29eb747d8de4ff93ed6f
sconkfinkle <101118943+sconkfinkle@users.noreply.github.com>
2022-08-22 16:48:04 +0530

    Add files via upload

--- hardhat-project/constants/index.js
+++ hardhat-project/constants/index.js
@@ -1,10 +1,16 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xc7e7595A5c85f8d2D87A89A0F65A54E47C5ebC78";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL ="https://github.com/sconkfinkle/NFT-Collection/tree/main/my-app/public/cryptodevs"
+    
+    //"https://nft-collection-sneh1999.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
 
 
 // compiled contract cryptodevs.sol
-// 0x77aa701583DAeE72A2905Fddd2D61D82Af913adF
+
+
+// 0xb4ca467C5EE9A402560cf755Fe047e0b7c33343c
+
+//0x77aa701583DAeE72A2905Fddd2D61D82Af913adF
